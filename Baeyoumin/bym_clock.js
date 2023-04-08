4b181c82ba748bb0b3ad91a38e79cde7b9d2e402
bum7706 <bum7706@naver.com>
2023-04-08 23:12:15 +0900

    1차^*^

---
+++ Baeyoumin/bym_clock.js
@@ -0,0 +1,26 @@
+function Clock(){
+    let nowTime = new Date();
+    let hour = add_0(nowTime.getHours());
+    let minute = add_0(nowTime.getMinutes());
+    let second = add_0(nowTime.getSeconds());
+    document.getElementById("clock").innerHTML = hour + ":" + minute + ":" + second;
+}
+
+Clock();
+
+function add_0(time){
+    if( parseInt(time) < 10 ) {
+        return "0" + time;
+    }
+    else {
+        return time;
+    }
+}
+function Datetoday() {
+    let today = new Date(); 
+    let year = today.getFullYear();   
+    let month = today.getMonth() + 1; 
+    let date = today.getDate(); 
+    document.getElementById("date").innerHTML =  year + "년" + month + "월" + date + "일"; 
+ }
+
