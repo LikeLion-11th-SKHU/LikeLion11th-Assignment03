a61c076550782c16b744c152123c7e34bddbe61c
sonamulovebunny <skit21sol@gmail.com>
2023-04-11 09:29:35 +0900

    흠

---
+++ imsol/imsol_clcok.js.js
@@ -0,0 +1,27 @@
+function add_0(time) {
+    if (parseInt(time) < 10) {
+        return "0" + time;
+    }
+    else {return time;}
+}
+
+function clock(){
+
+	let nowTime = new Date(); 
+	let hour = add_0(nowTime.getHours());
+	let minute = add_0(nowTime.getMinutes());
+	let second = add_0(nowTime.getSeconds());
+	document.getElementById("clock").innerHTML = hour + ":" + minute + ":" + second;
+}
+clock();
+
+function day() {
+
+    let today = new Date(); 
+    let year = today.getFullYear();   
+    let month = today.getMonth() + 1; 
+    let date = today.getDate(); 
+    document.getElementById("today").innerHTML =  year + "년 "+ month + "월 " +  date + "일"; 
+ }
+
+
